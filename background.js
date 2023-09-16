27864cc6a37fb6c9341a6236fdfc7065045e5a2d
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-09-16 17:29:33 -0400

    finished the ui styling basics and setup the entry point for alice's function

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "IMAGES") {
-        console.log("this is the array:",message.images);
-    }
-});
--- popup.js
+++ popup.js
@@ -1,9 +1,31 @@
 document.getElementById('scrapeImagesBtn').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        let currentTab = tabs[0];
-        chrome.scripting.executeScript({
-            target: {tabId: currentTab.id},
-            files: ['content.js']
+    if (this.id === "scrapeImagesBtn") {
+        console.log("Scraping images");
+        this.innerText = "Revert to Human";
+        this.id = "revertToHumanBtn";
+
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            let currentTab = tabs[0];
+
+            chrome.scripting.executeScript({
+                target: {tabId: currentTab.id},
+                files: ['content.js']
+            }, function() {
+                console.log("Content script injected");
+            });
+
         });
-    });
+
+        chrome.runtime.onMessage.addListener(function listener(message, sender, sendResponse) {
+            if (message.type === "IMAGES") {
+                console.log("Received images:", message.images);
+                chrome.runtime.onMessage.removeListener(listener);
+            }
+        });
+
+    } else if (this.id === "revertToHumanBtn") {
+        console.log("Reverting to Human");
+        this.innerText = "Revert to Animal";
+        this.id = "scrapeImagesBtn";
+    }
 });
